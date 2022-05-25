7d111b4418ad2d1a741815e0fbdfe14c9a03abc9
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-05-25 10:15:42 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:news_app/shared/cubit/app_cubit.dart';
 import 'package:news_app/shared/cubit/bloc_observer.dart';
 import 'package:news_app/shared/cubit_for_main/main_cubit.dart';
@@ -8,22 +9,23 @@ import 'package:news_app/shared/styles/colors.dart';
 
 import 'layouts/home.dart';
 
-
-
 void main() async {
   BlocOverrides.runZoned(
-        () async {
-      WidgetsFlutterBinding.ensureInitialized();
+    () async {
       //await GetStorage.init();
-      //await CacheHelper.init();
+      WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+      FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
       runApp(MyApp());
     },
     blocObserver: MyBlocObserver(),
   );
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key,}) : super(key: key);
+  MyApp({
+    Key? key,
+  }) : super(key: key);
   final UiColor theme = UiColor();
 
   @override
---
+++ lib/res/assets_res.dart
@@ -0,0 +1,8 @@
+/// Generated file. Do not edit.
+
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+class AssetsRes {
+  static const String LOGO = 'assets/Images/logo.png';
+  static const String LOGO_FOREGROUND = 'assets/Images/logo_foreground.png';
+}
