70d5786b1fa86d8c5f0380338c5bd62f859453da
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-05 21:28:06 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ import 'package:news_app/shared/cubit/app_cubit.dart';
 import 'package:news_app/shared/cubit/bloc_observer.dart';
 import 'package:news_app/shared/cubit_for_main/main_cubit.dart';
 import 'package:news_app/shared/cubit_for_main/main_state.dart';
-import 'package:news_app/shared/networks/local/cache_helper.dart';
-import 'package:news_app/shared/styles/colors.dart';
 import 'package:news_app/shared/styles/theme_service.dart';
 
 import 'layouts/home.dart';
