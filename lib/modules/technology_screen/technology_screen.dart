849ee3a9de81bf563792c48e1ad63ad7377759ed
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-08-24 01:38:56 +0200

    Update technology_screen.dart

--- lib/modules/technology_screen/technology_screen.dart
+++ lib/modules/technology_screen/technology_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:news_app/shared/cubit/app_cubit.dart';
 
 import '../../shared/components/components.dart';
@@ -11,25 +12,20 @@ class TechnologyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) =>
-      AppCubit()
-        ..getTechnologyDate(),
+      create: (context) => AppCubit()..getTechnologyDate(),
       child: BlocConsumer<AppCubit, AppState>(
         listener: (context, state) {},
         builder: (context, state) {
-          var list = AppCubit
-              .get(context)
-              .technology;
+          var list = AppCubit.get(context).technology;
           return Scaffold(
             body: list.isNotEmpty
                 ? ListView.separated(
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (context, index) => data(list[index],context),
-              separatorBuilder: (context, index) => separator(),
-              itemCount: list.length,
-            )
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (context, index) => data(list[index], context),
+                    separatorBuilder: (context, index) => separator(),
+                    itemCount: list.length,
+                  )
                 : circularProgressIndicator(),
-
           );
         },
       ),
