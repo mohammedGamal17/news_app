b7c251ba0098e5ced8dedcd14db428ca2800f44e
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-08-31 22:43:52 +0200

    Update technology_screen.dart

--- lib/modules/technology_screen/technology_screen.dart
+++ lib/modules/technology_screen/technology_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get_utils/get_utils.dart';
 import 'package:news_app/shared/cubit/app_cubit.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import '../../shared/components/components.dart';
 import '../../shared/cubit/app_state.dart';
@@ -15,16 +17,72 @@ class TechnologyScreen extends StatelessWidget {
       child: BlocConsumer<AppCubit, AppState>(
         listener: (context, state) {},
         builder: (context, state) {
-          var list = AppCubit.get(context).technology;
+          var cubit = AppCubit.get(context);
+          var list = cubit.technology;
           return Scaffold(
-            body: list.isNotEmpty
-                ? ListView.separated(
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (context, index) => data(list[index], context),
-                    separatorBuilder: (context, index) => separator(),
-                    itemCount: list.length,
-                  )
-                : circularProgressIndicator(),
+            body: ScreenTypeLayout(
+              mobile: Builder(
+                builder: (context) {
+                  cubit.getDesktop(false);
+                  return list.isNotEmpty
+                      ? ListView.separated(
+                          physics: const BouncingScrollPhysics(),
+                          itemBuilder: (context, index) =>
+                              data(list[index], context, index),
+                          separatorBuilder: (context, index) => separator(),
+                          itemCount: list.length,
+                        )
+                      : circularProgressIndicator();
+                },
+              ),
+              desktop: Builder(
+                builder: (context) {
+                  cubit.getDesktop(true);
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: list.isNotEmpty
+                            ? ListView.separated(
+                                physics: const BouncingScrollPhysics(),
+                                itemBuilder: (context, index) =>
+                                    data(list[index], context, index),
+                                separatorBuilder: (context, index) =>
+                                    separator(),
+                                itemCount: list.length,
+                              )
+                            : circularProgressIndicator(),
+                      ),
+                      if (list.isNotEmpty)
+                        Expanded(
+                          child: Container(
+                            color: Colors.grey[400],
+                            height: double.infinity,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Image(image: NetworkImage('${list[cubit.selectedItem]['urlToImage']}')),
+                                  SizedBox(height: 10.0),
+                                  Text(
+                                    '${list[cubit.selectedItem]['description']}',
+                                    style: TextStyle(fontSize: 18.0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  );
+                },
+              ),
+              breakpoints: ScreenBreakpoints(
+                desktop: 600.0,
+                tablet: 600.0,
+                watch: 100.0,
+              ),
+            ),
           );
         },
       ),
