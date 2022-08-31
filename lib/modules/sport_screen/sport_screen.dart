c3c5c7a78ea3b726e0dc22c8c4f6b35ae64a7b70
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-08-31 22:43:50 +0200

    Update sport_screen.dart

--- lib/modules/sport_screen/sport_screen.dart
+++ lib/modules/sport_screen/sport_screen.dart
@@ -24,7 +24,7 @@ class SportsScreen extends StatelessWidget {
               body: list.isNotEmpty
                   ? ListView.separated(
                 physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) => data(list[index],context),
+                itemBuilder: (context, index) => data(list[index],context,index),
                 separatorBuilder: (context, index) => separator(),
                 itemCount: list.length,
               )
